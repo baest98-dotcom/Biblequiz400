e255675fb87275a9756a083f6c4fb645f8c1041a
baest98-dotcom <baest98@gmail.com>
2026-06-29 11:12:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'bible-quiz-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'bible-quiz-v3';
+const FILES = [
+  '/Biblequiz400/',
+  '/Biblequiz400/index.html',
+  '/Biblequiz400/manifest.json',
+  '/Biblequiz400/icon-192.png',
+  '/Biblequiz400/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -17,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/Biblequiz400/index.html')))
   );
 });
